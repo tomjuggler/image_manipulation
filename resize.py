1964cfed6f4dcf917f0b52ed86e63c69848afcf8
Tom Hastings <tomjuggler@gmail.com>
2023-02-20 08:33:28 +0200

    resize width with height proportional
    Signed-off-by: Tom Hastings <tomjuggler@gmail.com>

---
+++ resize.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import sys
+import os
+
+
+def resize_image(file, fixed_width):
+    basewidth = fixed_width
+    im = Image.open(file)
+    wpercent = (basewidth/float(im.size[0]))
+    hsize = int((float(im.size[1])*float(wpercent)))
+    img = im.resize((basewidth,hsize), Image.Resampling.LANCZOS)
+
+    # Save the image as a new file
+    output_file_path = os.path.splitext(file)[0] + "_resized.png"
+    img.save(output_file_path, "PNG")
+    img.show() # todo: not for server version!
+
+if __name__ == "__main__":
+    # Get the path of the input file from the command-line arguments
+    input_file_path = sys.argv[1]
+
+    # Convert the binary file to an image
+    resize_image(input_file_path, 72)
