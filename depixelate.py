7cad516e92f56f207b32d1cb516dd7e6f17d0e07
Tom Hastings <tomjuggler@gmail.com>
2023-02-20 07:47:21 +0200

    cleaned up
    Signed-off-by: Tom Hastings <tomjuggler@gmail.com>

--- depixelate.py
+++ depixelate.py
@@ -2,29 +2,6 @@ import sys
 from PIL import Image
 import os
 
-def compress_image_to_8bit_color(image_path):
-    # Open the input image
-    input_image = Image.open(image_path)
-
-    binary_data = []
-
-    # Loop over each pixel in the input image and encode it
-    for x in range(input_image.width):
-        for y in range(input_image.height):
-            r, g, b = input_image.getpixel((x, y))
-            encoded = ((r & 0xE0) | ((g & 0xE0) >> 3) | (b >> 6))
-            binary_data.append(encoded)
-
-    # Calculate the output file path
-    output_file_path = os.path.splitext(image_path)[0] + ".bin"
-
-    # Convert the binary data to a binary file
-    with open(output_file_path, "wb") as binary_file:
-        binary_file.write(bytearray(binary_data))
-
-    return output_file_path
-
-
 def convert_8bit_color_to_image(bin_file_path):
     with open(bin_file_path, "rb") as bin_file:
         binary_data = bin_file.read()
@@ -45,7 +22,7 @@ def convert_8bit_color_to_image(bin_file_path):
     # Save the image as a new file
     output_file_path = os.path.splitext(bin_file_path)[0] + "_new.png"
     output_image.save(output_file_path, "PNG")
-    output_image.show() # todo: delete for server version!
+    output_image.show() # todo: not for server version!
 
 
 if __name__ == "__main__":
--- pixelate.py
+++ pixelate.py
@@ -6,9 +6,10 @@ def compress_image_to_8bit_color(image_path):
     # Open the input image
     input_image = Image.open(image_path)
     binary_data = []
-    # bytes = bytearray()
 
-
+    # todo: resize image to pre-defined poi size
+    # todo: rotate image 90 degrees for viewing on the poi 
+    
     # Loop over each pixel in the input image and encode it
     for x in range(input_image.height):
         for y in range(input_image.width):
@@ -16,20 +17,15 @@ def compress_image_to_8bit_color(image_path):
             encoded = ((r & 0xE0) | ((g & 0xE0) >> 3) | (b >> 6))
             print(encoded)
             binary_data.append(encoded)
-            # bytes.append(encoded)
-
+            
     # Convert the PNG file to a binary file
     with open(os.path.splitext(image_path)[0] + ".bin", "wb") as binary_file:
         binary_file.write(bytearray(binary_data))
-        # binary_file.write(bytes)
-
+        
 if __name__ == "__main__":
     # Get the path of the input file from the command-line arguments
     input_file_path = sys.argv[1]
 
     # Convert the binary file to an image
     compress_image_to_8bit_color(input_file_path)
-
-# compress_image_to_8bit_color("./nnn.png")
-
 
