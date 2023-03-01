525dfef09fc0bd3dc98a32eb55c0374bb654957a
Tom Hastings <tomjuggler@gmail.com>
2023-03-01 21:10:24 +0200

    rotate now working correctly in RotateImageVisualPoiStyle.py rotate.py using a.jpg which is in repo
    Signed-off-by: Tom Hastings <tomjuggler@gmail.com>

--- RotateImageVisualPoiStyle.py
+++ RotateImageVisualPoiStyle.py
@@ -11,13 +11,12 @@ def rotate_visual_poi_style(file, fixed_width):
     hsize = int((float(im.size[1])*float(wpercent)))
     img = im.resize((basewidth,hsize), Image.Resampling.LANCZOS)
     img_rotated = img.rotate(-90) # rotate 90 degrees clockwise 
-
     # start rotate image now: 
     fit = 1
     if (img_rotated.height > 360):
         fit = 1
     else:
-        fit = 360 // img.height # using original width - because height doesn't change after rotate! 
+        fit = 360 // img_rotated.height # using original width - because height doesn't change after rotate! # todo: rotate not working at all! 
 
     # create a blank image and rotate the input image onto it multiple times
     # to create the final spinning effect
@@ -26,17 +25,17 @@ def rotate_visual_poi_style(file, fixed_width):
     newImg = Image.new(mode="RGB", size=(400, 400)) # todo: change for different size poi? 
 
     # resize for rotation: 
-    img = img.resize((img_rotated.width, new_height), Image.Resampling.LANCZOS)
+    img_rotated = img_rotated.resize((img_rotated.width, new_height), Image.Resampling.LANCZOS)
 
     
     incrRotation = 0
     while incrRotation < 360:
-        w, h = img.size
+        w, h = img_rotated.size
         for y in range(h):
             for x in range(w):
                 adjacent = int((x+90) * math.sin(math.radians(-y+incrRotation)))
                 opposite = int((x+90) * math.cos(math.radians(-y+incrRotation)))
-                pixel_color = img.getpixel((x, y))
+                pixel_color = img_rotated.getpixel((x, y))
                 set_x = adjacent + (newImg.width // 2)
                 set_y = opposite + (newImg.height // 2)
                 newImg.putpixel((set_x, set_y), pixel_color)
@@ -51,4 +50,4 @@ if __name__ == "__main__":
     input_file_path = sys.argv[1]
 
     # Convert the binary file to an image
-    rotate_visual_poi_style(input_file_path, 72)
+    rotate_visual_poi_style(input_file_path, 100)
--- rotate.py
+++ rotate.py
@@ -1,10 +1,10 @@
 
 from PIL import Image
   
-Original_Image = Image.open("./nnn.png")
+Original_Image = Image.open("./a.jpg")
   
 # rotate image 90 degrees to the right
-rotated_image1 = Original_Image.rotate(-90) # that's right, it's counter-clockwise folks. Who thought up this bright idea? 
+rotated_image1 = Original_Image.rotate(-90) # that's right, positive means counter-clockwise folks. Who thought up this bright idea? 
   
 rotated_image1.show()
-rotated_image1.save("nnnRotated.png") # yes! easy with PIL
+rotated_image1.save("aRotated.png") # yes! easy with PIL
